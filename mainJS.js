3819c3980e4b77364decc47dd7e7660bc927ab31
rose.the.lexicon <140659755+rose-the-Lexicon@users.noreply.github.com>
2023-08-23 19:01:37 -0500

    Setup basic operations
    addition, subtraction, multiplication, and division.

--- mainJS.js
+++ mainJS.js
@@ -1,15 +1,69 @@
+let num1;
+let operator;
+let num2;
+let result;
+const screen = document.getElementById("screen");
+
 function operate (num, operator, numTwo) {
     num = parseInt(num);
     numTwo = parseInt(numTwo);
 
     if (operator === "+") {
+        screen.textContent = num + numTwo;
         return num + numTwo;
     } else if (operator === "-") {
+        screen.textContent = num - numTwo;
         return num - numTwo;
     } else if (operator === "*") {
+        screen.textContent = num * numTwo;
         return num * numTwo;
     } else if (operator === "/") {
+        screen.textContent = num / numTwo;
         return num / numTwo;
     }
 }
 
+function getCalculation () {
+    const equals = document.getElementById("=");
+    console.log(equals);
+    equals.addEventListener("click", () => {
+        num2 = parseFloat(screen.textContent);
+        operate(num1, operator, num2);
+    })
+}
+
+function screenUpdate () {
+    const numberButtons = document.querySelectorAll(".number");
+
+    numberButtons.forEach(element => {
+        element.addEventListener("click", () => {
+            if (screen.textContent === "+" ||
+            screen.textContent === "-" ||
+            screen.textContent === "/" ||
+            screen.textContent === "*") {
+                screen.textContent = (element.id);
+            } else {
+                screen.textContent += (element.id);
+            }
+        })
+    });
+}
+
+function operatorButtons () {
+    const operateButtons = document.querySelectorAll(".operator");
+
+    operateButtons.forEach(e => {
+        if (e.id !== "=") {
+            e.addEventListener("click", () => {
+                num1 = parseFloat(screen.textContent);
+                screen.textContent = (e.id);
+                operator = screen.textContent;
+            })
+        }
+    })
+}
+
+
+screenUpdate();
+operatorButtons();
+getCalculation();
